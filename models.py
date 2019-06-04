4c8a0dea7606606a5641d372d7a69617d0491e4f
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-06-04 16:40:26 +0900

    Update model for using cross entropy loss.

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class MLP(nn.Module):
 
     def __init__(self):
+        super().__init__()
         self.linear1 = nn.Linear(300 * 2, 200)
         self.linear2 = nn.Linear(200, 300)
         self.linear3 = nn.Linear(300, 2)
@@ -20,5 +21,5 @@ class MLP(nn.Module):
         concated = torch.cat((sent1s, sent2s), dim=1)  # [B, H * 2]
         x = self.relu(self.linear1(concated))
         x = self.relu(self.linear2(x))
-        out = self.softmax(self.linear3(x))
+        out = self.linear3(x)
         return out
