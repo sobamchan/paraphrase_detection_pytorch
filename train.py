e7073eb5bda6480eecfaffa8d4b1e12b3a17e2da
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-06-04 15:35:28 +0900

    Added training loop.

---
+++ train.py
@@ -0,0 +1,39 @@
+import fire
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from data import get
+from models import MLP
+
+
+def train(ddir: str, savedir: str, bsize: int, ft_path: str, use_cuda: bool, epoch: int, lr: float):
+    dataloader = get(ddir, savedir, bsize, ft_path)
+
+    device = torch.device('cuda' if use_cuda else 'cpu')
+    model = MLP()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criteria = nn.NLLLoss()
+
+    for i_epoch in range(1, epoch+1):
+        losses = []
+        for tgts, sent1s, sent2s in dataloader:
+            tgts = tgts.to(device)
+            sent1s = sent1s.to(device)
+            sent2s = sent2s.to(device)
+
+            preds = model(sent1s, sent2s)
+            loss = criteria(preds, tgts)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+        print(f'Train loss: {np.mean(losses)}')
+
+
+if __name__ == '__main__':
+    fire.Fire()
