a35bc5fcf5dfd22642cbf1971f6f3f408847300b
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-06-10 18:40:59 +0900

    Copied original models, train.

--- models.py
+++ models.py
@@ -4,28 +4,11 @@ import torch.nn as nn
 
 class MLP(nn.Module):
 
-    def __init__(self, trial):
+    def __init__(self):
         super().__init__()
-        self.layers = []
-        self.dropouts = []
-        nlayers = trial.suggest_int('nlayers', 3, 8)
-        dropout = trial.suggest_uniform('dropout', 0.2, 0.5)
-        input_dim = 300 * 2
-
-        for i in range(nlayers):
-            output_dim = int(trial.suggest_categorical(f'n_units_l{i}', list(range(100, 1000, 100))))
-            self.layers.append(nn.Linear(input_dim, output_dim))
-            self.dropouts.append(nn.Dropout(dropout))
-            input_dim = output_dim
-
-        self.layers.append(nn.Linear(input_dim, 2))
-
-        for idx, layer in enumerate(self.layers):
-            setattr(self, f'fc{idx}', layer)
-
-        for idx, dropout in enumerate(self.dropouts):
-            setattr(self, f'dropout{idx}', dropout)
-
+        self.linear1 = nn.Linear(300 * 2, 200)
+        self.linear2 = nn.Linear(200, 300)
+        self.linear3 = nn.Linear(300, 2)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
@@ -34,11 +17,9 @@ class MLP(nn.Module):
         sent1s: [B, H]
         sent2s: [B, H]
         '''
+
         concated = torch.cat((sent1s, sent2s), dim=1)  # [B, H * 2]
-
-        x = concated
-        for layer, dropout in zip(self.layers, self.dropouts):
-            x = self.relu(layer(x))
-            x = dropout(x)
-
-        return x
+        x = self.relu(self.linear1(concated))
+        x = self.relu(self.linear2(x))
+        out = self.linear3(x)
+        return out
--- train.py
+++ train.py
@@ -1,10 +1,9 @@
-# import sys
-# from datetime import datetime
-# from pathlib import Path
+import sys
+from datetime import datetime
+from pathlib import Path
 
 import fire
 import numpy as np
-import optuna
 
 import torch
 import torch.nn as nn
@@ -14,116 +13,86 @@ from data import get
 from models import MLP
 
 
-# def train(ddir: str, data_cache_dir: str, _savedir: str, bsize: int,
-#           ft_path: str, use_cuda: bool, epoch: int, lr: float, seed: int = 1111):
-def train(ddir: str, data_cache_dir: str, _savedir: str, bsize: int,
-          ft_path: str, use_cuda: bool, epoch: int, seed: int = 1111):
+def train(ddir: str, data_cache_dir: str, savedir: str, bsize: int,
+          ft_path: str, use_cuda: bool, epoch: int, lr: float, seed: int = 1111):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
 
-    # print('Loading dataset...')
+    savedir = Path(savedir)
+    savedir = savedir / datetime.now().strftime('%Y%m%d_%H%M%S')
+    savedir.mkdir()
+
+    logf = open(savedir / 'log.txt', 'w')
+    logf.write(' '.join(sys.argv) + '\n')
+
+    print('Loading dataset...')
     train_dataloader = get(ddir, data_cache_dir, bsize, ft_path, split='train')
     valid_dataloader = get(ddir, data_cache_dir, bsize, ft_path, split='valid')
 
-    def objective(trial):
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        np.random.seed(seed)
+    print('Setting up models...')
+    device = torch.device('cuda' if use_cuda else 'cpu')
+    model = MLP().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criteria = nn.CrossEntropyLoss()
 
-        # savedir = Path(_savedir)
-        # savedir = savedir / datetime.now().strftime('%Y%m%d_%H%M%S')
-        # savedir.mkdir()
+    best_acc = 0
 
-        # logf = open(savedir / 'log.txt', 'w')
-        # logf.write(' '.join(sys.argv) + '\n')
+    print('Start training...')
+    for i_epoch in range(1, epoch+1):
+        losses = []
+        model.train()
+        for tgts, sent1s, sent2s in train_dataloader:
+            tgts = tgts.to(device)
+            sent1s = sent1s.to(device)
+            sent2s = sent2s.to(device)
 
-        # optuna settings
-        lr = trial.suggest_uniform('lr', 1e-5, 1e-1)
+            preds = model(sent1s, sent2s)
+            loss = criteria(preds, tgts)
 
-        # print('Setting up models...')
-        device = torch.device('cuda' if use_cuda else 'cpu')
-        model = MLP(trial).to(device)
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-        criteria = nn.CrossEntropyLoss()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
 
-        best_acc = 0
-        n_fail_in_a_raw = 0
-        limit_n_fail_in_a_raw = 10
-
-        # print('Start training...')
-        for i_epoch in range(1, epoch+1):
-            losses = []
-            model.train()
-            for tgts, sent1s, sent2s in train_dataloader:
+        model.eval()
+        valid_losses = []
+        valid_accs = []
+        with torch.no_grad():
+            for tgts, sent1s, sent2s in valid_dataloader:
                 tgts = tgts.to(device)
                 sent1s = sent1s.to(device)
                 sent2s = sent2s.to(device)
 
                 preds = model(sent1s, sent2s)
+                pred_idxs = preds.argmax(dim=1).tolist()
+
                 loss = criteria(preds, tgts)
+                acc = len([1 for p, t in zip(pred_idxs, tgts.tolist()) if p == t]) / len(tgts.tolist())
+                valid_losses.append(loss.item())
+                valid_accs.append(acc)
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                losses.append(loss.item())
+        print(f'Train loss: {np.mean(losses)}')
+        logf.write(f'Train loss: {np.mean(losses)}\n')
 
-            model.eval()
-            valid_losses = []
-            valid_accs = []
-            with torch.no_grad():
-                for tgts, sent1s, sent2s in valid_dataloader:
-                    tgts = tgts.to(device)
-                    sent1s = sent1s.to(device)
-                    sent2s = sent2s.to(device)
+        _loss = np.mean(valid_losses)
+        _acc = np.mean(valid_accs)
+        print(f'Valid loss: {_loss}')
+        logf.write(f'Valid loss: {_loss}\n')
+        print(f'Valid accuracy: {_acc}')
+        logf.write(f'Valid accuracy: {_acc}\n')
 
-                    preds = model(sent1s, sent2s)
-                    pred_idxs = preds.argmax(dim=1).tolist()
+        if _acc > best_acc:
+            best_acc = _acc
+            print('Best acc')
+            print(f'Dumping the model to {savedir}...')
+            torch.save(model, savedir / 'best.pth')
 
-                    loss = criteria(preds, tgts)
-                    acc = len([1 for p, t in zip(pred_idxs, tgts.tolist()) if p == t]) / len(tgts.tolist())
-                    valid_losses.append(loss.item())
-                    valid_accs.append(acc)
+        print()
+        logf.write('\n')
 
-            # print(f'Train loss: {np.mean(losses)}')
-            # logf.write(f'Train loss: {np.mean(losses)}\n')
-
-            # _loss = np.mean(valid_losses)
-            _acc = np.mean(valid_accs)
-            # print(f'Valid loss: {_loss}')
-            # logf.write(f'Valid loss: {_loss}\n')
-            # print(f'Valid accuracy: {_acc}')
-            # logf.write(f'Valid accuracy: {_acc}\n')
-
-            if _acc > best_acc:
-                best_acc = _acc
-                n_fail_in_a_raw = 0
-                # print('Best acc')
-                # print(f'Dumping the model to {savedir}...')
-                # torch.save(model, savedir / 'best.pth')
-            else:
-                n_fail_in_a_raw += 1
-
-            if n_fail_in_a_raw >= limit_n_fail_in_a_raw:
-                break
-
-            # print()
-            # logf.write('\n')
-
-        # print(f'Dumping the model to {savedir}...')
-        # torch.save(model, savedir / 'model.pth')
-        return best_acc
-
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
-
-    print(f'Number of finished trials: {len(study.trials)}')
-    print('Best trial:')
-    trial = study.best_trial
-
-    print(f'    Value: {trial.value}')
-    print(f'    Params: ')
-    for k, v in trial.params.items():
-        print(f'      {k}: {v}')
-
-    # return objective
+    print(f'Dumping the model to {savedir}...')
+    torch.save(model, savedir / 'model.pth')
 
 
 if __name__ == '__main__':
