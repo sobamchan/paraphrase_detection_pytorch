2548863c2709023065221138efb394bb7796e8fb
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-06-11 15:25:34 +0900

    Added logger timestamp

--- logger.py
+++ logger.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from io import TextIOWrapper
 
 
 def get_logger(f: TextIOWrapper = None, to_print: bool = False):
 
     def _f(s: str, print_this_time: bool = False):
+        timestamp = f"[{datetime.now().strftime('%Y-%m-%d %H:%M%S')}]"
+        s = f'{timestamp} {s}'
         if f:
             f.write(s + '\n')
         if to_print:
